4ef478d47cc05fb417e58cf4f6789ca1e3e9d3f7
electroscientistX <bhoplepruthviraj@gmail.com>
2020-06-03 11:28:53 +0530

    Add files via upload

---
+++ screwChina.bat
@@ -0,0 +1,20 @@
+@echo OFF
+adb uninstall fi.harism.wallpaper.yinyang
+adb uninstall org.mrchops.android.digihud
+adb uninstall hunt.morseDit
+adb uninstall com.nilelabs.android.noisekiller
+adb uninstall net.openfiresecurity.ofsdoser
+adb uninstall net.sourceforge.opencamera
+adb uninstall android.androidVNC
+adb uninstall com.googlecode.chartdroid
+adb uninstall pete.android.morseit
+adb uninstall com.googlecode.python3forandroid
+adb uninstall com.googlecode.pythonforandroid
+adb uninstall com.dicon.sonar.ad
+adb uninstall com.vimtec.apps.facts
+COLOR 02
+pause
+echo CONGRATULATIONS!!!!! YOUR PHONE IS NOW CLEAN AND SAFE!
+pause
+echo for any doubt/suggestions/, go here: 
+pause
