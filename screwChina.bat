38b9efc4471d4b16bc558b7b446dadf03ab25e67
electroscientistX <bhoplepruthviraj@gmail.com>
2020-06-03 13:53:31 +0530

    Add files via upload

---
+++ screwChina.bat
@@ -0,0 +1,43 @@
+@echo OFF
+adb uninstall com.UCMobile.intl
+adb uninstall com.uc.browser.en
+adb uninstall com.ucturbo
+adb uninstall com.CricChat.intl
+adb uninstall com.zhiliaoapp.musically
+adb uninstall sg.bigo.live
+adb uninstall video.like
+adb uninstall chat.meme.inke
+adb uninstall com.asiainno.uplive
+adb uninstall com.youdao.hindict
+adb uninstall com.alibaba.aliexpresshd
+adb uninstall com.alibaba.intl.android.apps.poseidon
+adb uninstall com.alibaba.icbu.app.aliexpress.seller
+adb uninstall com.funcamerastudio.videomaker
+adb uninstall com.quvideo.xiaoying
+adb uninstall com.tencent.ig
+adb uninstall com.tencent.iglite
+adb uninstall naturephotoframe.naturephotoeditor
+adb uninstall com.commsource.beautyplus
+adb uninstall com.pdfscanner.documentscanner
+adb uninstall com.meitu.makeup
+adb uninstall com.videochat.livu
+adb uninstall com.hkfuliao.chamet
+adb uninstall com.yy.hiyo
+adb uninstall com.huya.pokogame
+adb uninstall com.match.zhayan
+adb uninstall cn.xender
+adb uninstall com.domobile.applockwatcher
+adb uninstall cleanmaster.phone.memory.booster.cleaner
+adb uninstall com.cleanteam.oneboost
+adb uninstall com.katecca.screenofflock
+adb uninstall com.nqmobile.antivirus20
+adb uninstall com.xprodev.cutcam
+adb uninstall com.mi.global.shop
+adb uninstall com.allinone.callerid
+adb uninstall com.cam001.selfie
+COLOR 02
+pause
+echo CONGRATULATIONS!!!!! YOUR PHONE IS NOW CLEAN AND SAFE!
+pause
+echo for any doubt/suggestions/, go here: 
+pause
